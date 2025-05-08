054538fe43989732156d8992cc112eea54cc8e02
jennydev <jennydev2001@gmail.com>
2025-05-08 11:11:52 +0545

    lazy loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,20 +6,29 @@ import Signup from './Signup'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './Login'
 
-import Quote from './Quote'
-import Home from './HOme'
+//import Quote from './Quote'
+import Home from './Home'
+const Quote = React.lazy(()=>import('./Quote'))
 
 function App() {
   
 
   return (
    <BrowserRouter>
+    
    <Routes>
     <Route path='/register' element={<Signup/>} />
     <Route path='/login' element={<Login/>} />
     <Route path='/' element={<Home/>} />
-    <Route path='/quote' element={<Quote/>} />
+   
+    <Route path='/quote' element={
+      <Suspense fallback={<div>Loading...</div>}>
+        <Quote/>
+        </Suspense>} 
+    />
+    
    </Routes> 
+   
    </BrowserRouter>
   )
 }
